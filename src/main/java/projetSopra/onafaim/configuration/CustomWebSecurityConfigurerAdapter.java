efadc56472118a546d3e6f66933463db6847907b
lilia.dai@yahoo.fr <lilia.dai@yahoo.fr>
2020-02-04 15:26:47 +0100

    CustomWebSecurityConfigurerAdapter en cours

--- src/main/java/projetSopra/onafaim/configuration/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/projetSopra/onafaim/configuration/CustomWebSecurityConfigurerAdapter.java
@@ -1,5 +1,23 @@
 package projetSopra.onafaim.configuration;
 
-public class CustomWebSecurityConfigurerAdapter {
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource datasource; 
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		
+	}
+	
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
