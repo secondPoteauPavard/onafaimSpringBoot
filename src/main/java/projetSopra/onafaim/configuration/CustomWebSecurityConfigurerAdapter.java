62e7301df160baefdf3c9ba8538415849960e108
lilia.dai@yahoo.fr <lilia.dai@yahoo.fr>
2020-02-04 16:18:00 +0100

    CustomWebSecurityConfigurerAdapter et View Inscription

--- src/main/java/projetSopra/onafaim/configuration/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/projetSopra/onafaim/configuration/CustomWebSecurityConfigurerAdapter.java
@@ -1,22 +1,37 @@
 package projetSopra.onafaim.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource datasource; 
+	private UserDetailsService userDetailsService; 
 	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.authorizeRequests().antMatchers("/auth/admin/**").hasAnyRole("ADMIN")
+		.and().formLogin().loginPage("/login")
+		.and().authorizeRequests().antMatchers("/auth/**").authenticated()
+		.and().formLogin().loginPage("/login").permitAll()
+		.and().logout().logoutSuccessUrl("/accueil")
+		.and().authorizeRequests().antMatchers("/accueil/**").permitAll();
 	}
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()); 	
+	}
+	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
