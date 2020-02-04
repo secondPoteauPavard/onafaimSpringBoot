fa6e6abaf9b7376482e0cae34232370a35ae16b7
Lovelyse Lete Zitongo <lovelysedu94@hotmail.fr>
2020-02-04 17:18:40 +0100

    test Compte en cours

--- src/main/java/projetSopra/onafaim/restController/CompteRestController.java
+++ src/main/java/projetSopra/onafaim/restController/CompteRestController.java
@@ -1,5 +1,7 @@
 package projetSopra.onafaim.restController;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import projetSopra.onafaim.model.Compte;
 import projetSopra.onafaim.repositories.CompteRepository;
 
 @RestController
+@RequestMapping("/rest")
 public class CompteRestController {
 
 	@Autowired
@@ -35,12 +40,25 @@ public class CompteRestController {
 		return new ResponseEntity<Compte>(HttpStatus.NOT_IMPLEMENTED);
 	}
 	
-//	@PostMapping("/page/editCompte/{id}")
-//	public ResponseEntity<Compte> update(@PathVariable("id") Long id, @RequestBody @Valid Compte compte, BindingResult br){
-//		
-//	}
+	@PutMapping("/page/editCompte/{id}")
+	public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody @Valid Compte compte, BindingResult br){
+		Optional<Compte> opt=compteRepository.findById(id);
+		if(opt.isPresent()) {
+			compteRepository.save(compte);
+			return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
+		}
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
 	
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+		Optional<Compte> opt=compteRepository.findById(id);
+		if(opt.isPresent()) {
+			compteRepository.deleteById(id);
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
 	
 	
 }
