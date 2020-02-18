96cd7316cbcea9dc44e27eff2c1b16e2032b6767
unknown <thibautlesueur@hotmail.fr>
2020-02-18 09:49:08 +0100

    thibaut

--- src/main/java/projetSopra/onafaim/restController/CompteRestController.java
+++ src/main/java/projetSopra/onafaim/restController/CompteRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import projetSopra.onafaim.repositories.CompteRepository;
 
 @RestController
 @RequestMapping("/rest")
+@CrossOrigin(origins = {"*"})
 public class CompteRestController {
 
 	@Autowired
